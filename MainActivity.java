746c9ce2d4bffca57d2fc719192930929058a4ff
ShahinAlomBijoy <70821773+ShahinAlomBijoy@users.noreply.github.com>
2021-06-10 14:56:22 +0600

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mynavigation;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+DrawerLayout drawerLayout;
+ActionBarDrawerToggle actionBarDrawerToggle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        drawerLayout=findViewById(R.id.drawerId);
+        actionBarDrawerToggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.nv_open,R.string.nv_close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(actionBarDrawerToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
